871bb6c69df6f78c0e1dd2b32264f3968091c45f
iiestIT <it.iiest.de@gmail.com>
2020-01-14 19:45:00 +0100

    picture to metadata_tags rdy

--- analyse/code/analyse.py
+++ analyse/code/analyse.py
@@ -1,6 +1,46 @@
-from bs4 import BeautifulSoup
-
-import time
+import exifread
+import datetime
+import json
 import os
-import sys
 
+
+def data(img_input, name="default"):
+    now = datetime.datetime.now()
+    date_time = now.strftime("%H-%M-%S_%m-%d-%Y")
+    
+    img = open(img_input, "rb")
+    tags = exifread.process_file(img)
+    tags.update({'time_now' : date_time})
+    tags.update({'file_name' : name})
+
+    for key, value in tags.items():
+        print(str(key) + ": " + str(value))
+
+    try:
+        with open("cache.txt", "w") as file:
+            file.write("cache_dict" + " = " + str(tags))
+
+            
+            
+    except IOError:
+        print(IOError)
+
+'''
+with open("test.jpg", "rb") as img:
+    tags = exifread.process_file(img)
+    for key, value in tags.items():
+        print(str(key) + " - " + str(value))
+        '''
+
+data('test.jpg')
+
+"""
+def data(img_input):
+    img = open(img_input, "rb")
+    tags = exifread.process_file(img)
+    try:
+        for key, value in tags.items():
+            print(str(key) + " - " + str(value))
+            
+    except IOError:
+        print("Error")"""
