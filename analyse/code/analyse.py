ae6732bf90db079014d1849cf0c4d2b6a32e831b
iiestIT <it.iiest.de@gmail.com>
2020-01-16 11:32:53 +0100

    test

--- analyse/code/analyse.py
+++ analyse/code/analyse.py
@@ -42,5 +42,3 @@ def data(img_input, name="default"):
 
     except IOError:                                                                                             #TODO write a errorhandler
         print(IOError)
-
-data('test.jpg')                                                                                                #? only the test
