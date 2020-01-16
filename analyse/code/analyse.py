abd908265db8a9007b76cad61c34c8e470d07457
iiestIT <it.iiest.de@gmail.com>
2020-01-16 08:28:05 +0100

    backend rdy

--- analyse/code/analyse.py
+++ analyse/code/analyse.py
@@ -1,46 +1,46 @@
+#? imports
 import exifread
 import datetime
-import json
 import os
+import copy
 
-
+# ? main-function
 def data(img_input, name="default"):
-    now = datetime.datetime.now()
-    date_time = now.strftime("%H-%M-%S_%m-%d-%Y")
-    
-    img = open(img_input, "rb")
-    tags = exifread.process_file(img)
-    tags.update({'time_now' : date_time})
-    tags.update({'file_name' : name})
+    if name == "default":                                                                                       #? get the right name
+        name = img_input
 
-    for key, value in tags.items():
-        print(str(key) + ": " + str(value))
+    now = datetime.datetime.now()                                                                               #? get a timestamp
+    date_time = now.strftime("%H-%M-%S_%m-%d-%Y")                                                               #? timestamp-format = h, m, s - m, d, y
+    
+    cache_dir = "./cache/"                                                                                      #? cache directory (path)
+    path_name = name + "_" + date_time                                                                          #? filename + timestamp = path-name
+    path = os.path.join(cache_dir, path_name)                                                                   #? path = cach_dir + path_name
+    
+    img = open(img_input, "rb")                                                                                 #? open input in b as img
+    tags = exifread.process_file(img)                                                                           #? read the exif-data and create a dict named tags
+    tags.update({'time_now' : date_time})                                                                       #? timestamp is joyning the dict
+    tags.update({'file_name' : name})                                                                           #? filename is joyning the dict
+
+    JPEGThumbnail = copy.deepcopy(tags["JPEGThumbnail"])                                                        #? JPEGThumbnail is copyed from the dict
+    del tags["JPEGThumbnail"]                                                                                   #? JPEGThumbnail is del from the dict
 
     try:
-        with open("cache.txt", "w") as file:
-            file.write("cache_dict" + " = " + str(tags))
-
-            
-            
-    except IOError:
+        with open(path + "_" + "JPEGThumbnail.txt", "w") as thumbnail:                                          #? write the JPEGThumbnail in the cache folder as an own file
+            thumbnail.write(str(JPEGThumbnail))
+    except IOError:                                                                                             #TODO write a errorhandler
         print(IOError)
 
-'''
-with open("test.jpg", "rb") as img:
-    tags = exifread.process_file(img)
-    for key, value in tags.items():
-        print(str(key) + " - " + str(value))
-        '''
-
-data('test.jpg')
-
-"""
-def data(img_input):
-    img = open(img_input, "rb")
-    tags = exifread.process_file(img)
     try:
-        for key, value in tags.items():
-            print(str(key) + " - " + str(value))
-            
-    except IOError:
-        print("Error")"""
+        with open(path + ".json", "a") as file:                                                                 #? create the json file with path + .json
+            file.write("{" + "\n")                                                                              #? write the first line of json-format
+            for key, value in tags.items():                                                                     #? loop the dict tags and write it into the file
+                if key != "file_name":                                                                          #? TRUE for all vals exept file_name
+                    file.write("    " + '"' + str(key) + '"' + " : " + '"' + str(value) + '",' + "\n")          #? format of the lines
+                else:                                                                                           #? only file_name
+                    file.write("    " + '"' + str(key) + '"' + " : " + '"' + str(value) + '"' + "\n")           #? same like above without the last ,
+            file.write("}" + "\n")                                                                              #? last character in the json-format
+
+    except IOError:                                                                                             #TODO write a errorhandler
+        print(IOError)
+
+data('test.jpg')                                                                                                #? only the test
